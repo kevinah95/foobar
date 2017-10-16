3aac91cdfc8f4d64a822373e8536a7fd27eb7f58
Kevin Hernandez Rostran <kevinah95@gmail.com>
2017-10-16 16:42:05 -0600

    Challenge 02 - Elevator Maintenance

---
+++ 02/elevator_maintenance/solution.py
@@ -0,0 +1,9 @@
+def answer(l):
+    l.sort(key=lambda s: list(map(int, s.split('.'))))
+    return l
+
+# l = ["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"]
+# print(answer(l))
+
+# l = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
+# print(answer(l))
