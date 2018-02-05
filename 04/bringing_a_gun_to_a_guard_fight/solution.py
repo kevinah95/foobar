889d4c88e6eccbd6bfd61539943b15652aebe35b
Kevin Hernández <kevinah95@gmail.com>
2018-02-05 00:07:00 -0600

    bringing_a_gun_to_a_guard_fight

---
+++ 04/bringing_a_gun_to_a_guard_fight/solution.py
@@ -0,0 +1,53 @@
+import math
+
+BOTTOM_LEFT = (0, 0)
+TOP_RIGHT = (3, 2)
+
+SOURCE = (1, 1)
+TARGET = (2, 1)
+
+def get_mirrored(point):
+    ret = []
+
+    # mirror at top wall
+    ret.append((point[0], point[1] - 2*(point[1] - TOP_RIGHT[1])))
+    # mirror at bottom wall
+    ret.append((point[0], point[1] - 2*(point[1] - BOTTOM_LEFT[1])))
+    # mirror at left wall
+    ret.append((point[0] - 2*(point[0] - BOTTOM_LEFT[0]), point[1]))
+    # mirror at right wall
+    ret.append((point[0] - 2*(point[0] - TOP_RIGHT[0]), point[1]))
+    return ret
+
+#print(get_mirrored(TARGET))
+
+def get_targets(start_point, distance):
+
+    all_targets = set((start_point, ))  # will also be the return value
+    last_targets = all_targets          # need to memorize the new points
+
+    while True:
+        new_level_targets = set()  # if this is empty: break the loop
+        for tgt in last_targets:   # loop over what the last iteration found
+            new_targets = get_mirrored(tgt)
+            # only keep the ones within range
+            new_targets = set(
+                t for t in new_targets
+                if math.hypot(SOURCE[0]-t[0], SOURCE[1]-t[1]) <= DISTANCE)
+            # subtract the ones we already have
+            new_targets -= all_targets
+            new_level_targets |= new_targets
+        if not new_level_targets:
+            break
+        # add the new targets
+        all_targets |= new_level_targets
+        last_targets = new_level_targets  # need these for the next iteration
+
+    return all_targets
+
+DISTANCE = 4    
+
+all_targets = get_targets(start_point=TARGET, distance=DISTANCE)
+print( (all_targets))
+
+# [1, 0], [1, 2], [1, -2], [3, 2], [3, -2], [-3, 2], and [-3, -2]
